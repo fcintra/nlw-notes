ba735eb8e39a8e57a2f5746a496bbab56fc20629
fcintra <matheus.cintra_2002@hotmail.com>
2024-02-11 20:17:57 -0300

    feat: add lib radixUI

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,3 +1,4 @@
+import * as Dialog from "@radix-ui/react-dialog";
 
 interface NoteCardProps {
     note: {
@@ -8,11 +9,19 @@ interface NoteCardProps {
 
 export function NoteCard({note}: NoteCardProps){
     return (
-        <button className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 outline-none overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-600">
-            <span className="taxt-sm font-medium text-slate-300">{note.date.toISOString()}</span>
-            <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+        <Dialog.Root>
+            <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 outline-none overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-600">
+                <span className="taxt-sm font-medium text-slate-300">{note.date.toISOString()}</span>
+                <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
-            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"/>
-        </button>
+                <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"/>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+                <Dialog.Content>
+                    a
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
     )
 }
