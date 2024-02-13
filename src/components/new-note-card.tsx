8f5e9fafd542e4216cbb074ee6be98de329b8c03
fcintra <matheus.cintra_2002@hotmail.com>
2024-02-12 22:06:24 -0300

    refactor: button save note

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -25,20 +25,17 @@ export function NewNoteCard({onNoteCreated}: NewNoteCardProps){
             setShouldShowOnboarding(true);
         }
     }
-
-                    
-                    
+                
     function handleSaveNote(event: FormEvent) {
         event.preventDefault();
 
         if(content === '') return toast.error('Digite algo!');
 
         onNoteCreated(content);
-
         setContent('');
         setShouldShowOnboarding(true);
 
-        toast.success('Nota criada com sucesso');
+        toast.success('Nota criada com sucesso!');
     }
 
     function handleStartRecording(){
@@ -63,6 +60,7 @@ export function NewNoteCard({onNoteCreated}: NewNoteCardProps){
             speechRecognition.maxAlternatives = 1;
             speechRecognition.interimResults = true;
 
+           
             speechRecognition.onresult = (event) => {
                 const trascription = Array.from(event.results).reduce((text, result) => {
                     return text.concat(result[0].transcript)
@@ -91,9 +89,10 @@ export function NewNoteCard({onNoteCreated}: NewNoteCardProps){
             speechRecognition.stop(); 
         }
     }
+      
 
     return (
-        <Dialog.Root>
+        <Dialog.Root defaultOpen>
             <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 text-left p-5 gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-600">
                 <span className="taxt-sm font-medium text-slate-200">Adicionar nota</span>
                 <p className="text-sm leading-6 text-slate-400">Grave uma nota em áudio que será convertida para texto automaticamente.</p>  
@@ -136,7 +135,6 @@ export function NewNoteCard({onNoteCreated}: NewNoteCardProps){
                             </button>
                         ) : (
                             <button 
-                                disabled={content === '' ? true : false}
                                 type="button"
                                 onClick={handleSaveNote}
                                 className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
